f7496a14722f6500de58fb75166b9599ee9f2473
Andy Berry <andyberry88@gmail.com>
2014-10-30 21:47:42 +0000

    adding a usage tracker that logs to KeenIO

--- brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageKeenIOEventObserver.java
+++ brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageKeenIOEventObserver.java
@@ -24,7 +24,7 @@ import org.bladerunnerjs.plugin.base.AbstractPlugin;
 import org.eclipse.jetty.client.HttpClient;
 
 
-public class BRJSUsageFirebaseEventObserver extends AbstractModelObserverPlugin implements EventObserver
+public class BRJSUsageKeenIOEventObserver extends AbstractModelObserverPlugin implements EventObserver
 {
 	
 	private BRJS brjs;
@@ -62,7 +62,8 @@ public class BRJSUsageFirebaseEventObserver extends AbstractModelObserverPlugin 
 			
 			String jsonBlob = UsageTrackingRestPayloadBuilder.bundlesetPayload(lastCreationStartTime, bundleSetCreatedEvent.getBundleSet());
 			
-			HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/bundlesets.json");
+			HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/bundlesets?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+			firebasePost.setHeader("Content-Type", "application/json");
 			try
 			{
 				firebasePost.setEntity( new StringEntity(jsonBlob) );
@@ -79,7 +80,8 @@ public class BRJSUsageFirebaseEventObserver extends AbstractModelObserverPlugin 
 			
 			String jsonBlob = UsageTrackingRestPayloadBuilder.commandPayload(brjs, commandExecutedEvent.getCommand());
 			
-			HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/commands.json");
+			HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/commands?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+			firebasePost.setHeader("Content-Type", "application/json");
 			try
 			{
 				firebasePost.setEntity( new StringEntity(jsonBlob) );
@@ -94,7 +96,8 @@ public class BRJSUsageFirebaseEventObserver extends AbstractModelObserverPlugin 
         } else if (event instanceof NewInstallEvent) {        	
         	String jsonBlob = UsageTrackingRestPayloadBuilder.newInstallPayload(brjs);
         	
-        	HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/installs.json");
+        	HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/installs?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+        	firebasePost.setHeader("Content-Type", "application/json");
         	try
         	{
         		firebasePost.setEntity( new StringEntity(jsonBlob) );

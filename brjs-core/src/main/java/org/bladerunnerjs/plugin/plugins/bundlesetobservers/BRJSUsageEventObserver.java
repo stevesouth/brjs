9d85274ba767fdea565ba730ed179d08e8981f21
Andy Berry <andyberry88@gmail.com>
2014-11-02 11:24:36 +0000

    combining the two stats plugins so we can controll API usage order

--- brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageEventObserver.java
+++ brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageEventObserver.java
@@ -24,7 +24,7 @@ import org.bladerunnerjs.plugin.base.AbstractPlugin;
 import org.eclipse.jetty.client.HttpClient;
 
 
-public class BRJSUsageKeenIOEventObserver extends AbstractModelObserverPlugin implements EventObserver
+public class BRJSUsageEventObserver extends AbstractModelObserverPlugin implements EventObserver
 {
 	
 	private BRJS brjs;
@@ -55,8 +55,19 @@ public class BRJSUsageKeenIOEventObserver extends AbstractModelObserverPlugin im
 			}
 		}
 		
-		new Thread(new EventHandlerThread(event, node)).start();
+		Thread t = new Thread(new EventHandlerThread(event, node));
+		t.start();
+		try
+		{
+			t.join();
+		}
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
 	
 	private class EventHandlerThread implements Runnable {
 
@@ -76,57 +87,76 @@ public class BRJSUsageKeenIOEventObserver extends AbstractModelObserverPlugin im
 				
 				String jsonBlob = UsageTrackingRestPayloadBuilder.bundlesetPayload(lastCreationStartTime, bundleSetCreatedEvent.getBundleSet());
 				
-				HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/bundlesets?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-				firebasePost.setHeader("Content-Type", "application/json");
+				HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/bundlesets?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+				keenIOPost.setHeader("Content-Type", "application/json");
+				
+				HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/bundlesets.json");
 				try
 				{
-					firebasePost.setEntity( new StringEntity(jsonBlob) );
+					keenIOPost.setEntity( new StringEntity(jsonBlob) );
 					DefaultHttpClient client = new DefaultHttpClient();
+					client.execute(keenIOPost);
+					
+					firebasePost.setEntity( new StringEntity(jsonBlob) );
+					client = new DefaultHttpClient();
 					client.execute(firebasePost);
 				}
 				catch (Exception e)
 				{
 					brjs.logger(this.getClass()).error(e.toString());
-					throw new RuntimeException(e);
 				}
 			} else if (event instanceof CommandExecutedEvent) {
 				CommandExecutedEvent commandExecutedEvent = (CommandExecutedEvent) event;
 				
 				String jsonBlob = UsageTrackingRestPayloadBuilder.commandPayload(brjs, commandExecutedEvent.getCommand());
 				
-				HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/commands?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-				firebasePost.setHeader("Content-Type", "application/json");
+				HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/commands?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+				keenIOPost.setHeader("Content-Type", "application/json");
+				
+				HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/commands.json");
 				try
 				{
-					firebasePost.setEntity( new StringEntity(jsonBlob) );
+					keenIOPost.setEntity( new StringEntity(jsonBlob) );
 					DefaultHttpClient client = new DefaultHttpClient();
+					client.execute(keenIOPost);
+					
+					firebasePost.setEntity( new StringEntity(jsonBlob) );
+					client = new DefaultHttpClient();
 					client.execute(firebasePost);
 				}
 				catch (Exception e)
 				{
 					brjs.logger(this.getClass()).error(e.toString());
-					throw new RuntimeException(e);
 				}
 	        } else if (event instanceof NewInstallEvent) {        	
 	        	String jsonBlob = UsageTrackingRestPayloadBuilder.newInstallPayload(brjs);
 	        	
-	        	HttpPost firebasePost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/installs?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-	        	firebasePost.setHeader("Content-Type", "application/json");
+	        	HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/installs?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+	        	keenIOPost.setHeader("Content-Type", "application/json");
+	        	
+	        	HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/installs.json");
 	        	try
 	        	{
-	        		firebasePost.setEntity( new StringEntity(jsonBlob) );
+	        		keenIOPost.setEntity( new StringEntity(jsonBlob) );
 	        		DefaultHttpClient client = new DefaultHttpClient();
-	        		client.execute(firebasePost);
+	        		client.execute(keenIOPost);
+	        		
+	        		firebasePost.setEntity( new StringEntity(jsonBlob) );
+					client = new DefaultHttpClient();
+					client.execute(firebasePost);
 	        	}
 	        	catch (Exception e)
 	        	{
 	        		brjs.logger(this.getClass()).error(e.toString());
-	        		throw new RuntimeException(e);
 	        	}
-	    	}			
+	    	}
+			
+			
+			
+			
 		}
 		
 	}
-
+	
 
 }

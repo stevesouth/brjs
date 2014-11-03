791596056abbef2f57892a5e0cb784557cebb00e
leggetter <phil@leggetter.co.uk>
2014-11-03 13:59:16 +0000

    Refactor into shared trackEvent method

--- brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageEventObserver.java
+++ brjs-core/src/main/java/org/bladerunnerjs/plugin/plugins/bundlesetobservers/BRJSUsageEventObserver.java
@@ -87,73 +87,38 @@ public class BRJSUsageEventObserver extends AbstractModelObserverPlugin implemen
 				
 				String jsonBlob = UsageTrackingRestPayloadBuilder.bundlesetPayload(lastCreationStartTime, bundleSetCreatedEvent.getBundleSet());
 				
-				HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/bundlesets?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-				keenIOPost.setHeader("Content-Type", "application/json");
-				
-				HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/bundlesets.json");
-				try
-				{
-					keenIOPost.setEntity( new StringEntity(jsonBlob) );
-					DefaultHttpClient client = new DefaultHttpClient();
-					client.execute(keenIOPost);
-					
-					firebasePost.setEntity( new StringEntity(jsonBlob) );
-					client = new DefaultHttpClient();
-					client.execute(firebasePost);
-				}
-				catch (Exception e)
-				{
-					brjs.logger(this.getClass()).error(e.toString());
-				}
+				trackEvent(jsonBlob, "bundlesets");
 			} else if (event instanceof CommandExecutedEvent) {
 				CommandExecutedEvent commandExecutedEvent = (CommandExecutedEvent) event;
 				
 				String jsonBlob = UsageTrackingRestPayloadBuilder.commandPayload(brjs, commandExecutedEvent.getCommand());
 				
-				HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/commands?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-				keenIOPost.setHeader("Content-Type", "application/json");
-				
-				HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/commands.json");
-				try
-				{
-					keenIOPost.setEntity( new StringEntity(jsonBlob) );
-					DefaultHttpClient client = new DefaultHttpClient();
-					client.execute(keenIOPost);
-					
-					firebasePost.setEntity( new StringEntity(jsonBlob) );
-					client = new DefaultHttpClient();
-					client.execute(firebasePost);
-				}
-				catch (Exception e)
-				{
-					brjs.logger(this.getClass()).error(e.toString());
-				}
+				trackEvent(jsonBlob, "commands");
 	        } else if (event instanceof NewInstallEvent) {        	
 	        	String jsonBlob = UsageTrackingRestPayloadBuilder.newInstallPayload(brjs);
 	        	
-	        	HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/installs?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
-	        	keenIOPost.setHeader("Content-Type", "application/json");
-	        	
-	        	HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/installs.json");
-	        	try
-	        	{
-	        		keenIOPost.setEntity( new StringEntity(jsonBlob) );
-	        		DefaultHttpClient client = new DefaultHttpClient();
-	        		client.execute(keenIOPost);
-	        		
-	        		firebasePost.setEntity( new StringEntity(jsonBlob) );
-					client = new DefaultHttpClient();
-					client.execute(firebasePost);
-	        	}
-	        	catch (Exception e)
-	        	{
-	        		brjs.logger(this.getClass()).error(e.toString());
-	        	}
+	        	trackEvent(jsonBlob, "installs");
 	    	}
-			
-			
-			
-			
+		}
+
+		private void trackEvent(String jsonBlob, String eventType) {
+			try
+			{
+				HttpPost keenIOPost = new HttpPost("https://api.keen.io/3.0/projects/5452adc733e406748303ecb4/events/" + eventType + "?api_key=2788245138ec539bd1e5fbb7fbb9d015b700d0a1687eaca22cb60abf8ce9bcd3515d1a81b7400b0b3459ab796357dc8629192380b1fd9286667eb958eb68a2e3ae58ecaff480a7eca3a49f9bdad43c3f86cc6636ce5c43ac83038b867f91f7e4426c9006809a26113e2709d1f7fb524a");
+				keenIOPost.setHeader("Content-Type", "application/json");
+				keenIOPost.setEntity( new StringEntity(jsonBlob) );
+				DefaultHttpClient client = new DefaultHttpClient();
+				client.execute(keenIOPost);
+				
+				HttpPost firebasePost = new HttpPost("https://brjs-usage-dashboard.firebaseio.com/" + eventType + ".json");
+				firebasePost.setEntity( new StringEntity(jsonBlob) );
+				client = new DefaultHttpClient();
+				client.execute(firebasePost);
+			}
+			catch (Exception e)
+			{
+				brjs.logger(this.getClass()).error(e.toString());
+			}
 		}
 		
 	}
